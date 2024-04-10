0d28be41ade135f252b57eeac711ef6a01def917
wowok <enoloo@163.com>
2024-04-10 12:15:06 +0800

    bug fix

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ npx tsx .\src\index.ts
 
 #### Test with your priv-key & address
 ```
-set ADDR_B64 = { your address }
-process.env.ADDR_B64
-set PRIV_B64 = { your private key }
-process.env.PRIV_B64
+set ADDR = { your address }
+process.env.ADDR_
+set PRIV = { your private key }
+process.env.PRIV
 ```
 
--- src/common.ts
+++ src/common.ts
@@ -1,14 +1,14 @@
 
 export const TEST_ADDR = (): string => {  
-    if (process.env.ADDR_B64) {
-        return process.env.ADDR_B64 as string 
+    if (process.env.ADDR) {
+        return process.env.ADDR as string 
     } else {
         return "0xe386bb9e01b3528b75f3751ad8a1e418b207ad979fea364087deef5250a73d3f"
     }
 }
 export const TEST_PRIV = (): string => {  
-    if (process.env.ADDR_B64) {
-        return process.env.PRIV_B64 as string 
+    if (process.env.PRIV) {
+        return process.env.PRIV as string 
     } else {
         return "0xc9bbc30f72ef7d9aa674a3be1448b9267141a676b59f3f4315231617a5bbc0e8"
     }
